8004ae0e8ae1b9e204f0df4342663f2980bcff7f
Tom Ross <tromik@gmail.com>
2017-08-13 11:09:42 -0400

    Field-to-datatype mappings

---
+++ mappings.py
@@ -0,0 +1,7 @@
+field_data_type_mappings = {
+    'CharField': 'text',
+    'IntegerField': 'integer',
+    'BoolField': 'boolean',
+    'DateField': 'date',
+    'DatetimeField': 'timestamp without timezone'
+}
